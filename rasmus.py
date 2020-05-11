1318eb9643cafe05acb21955884821066aba693e
johann <johann.suarez92@gmail.com>
2020-05-10 18:42:45 -0700

    Added exception handling in case the password field in configst.txt is empty.

--- rasmus.py
+++ rasmus.py
@@ -19,7 +19,7 @@ name_str = conf.readline()
 host_str = conf.readline()
 private_key_str = conf.readline()
 pass_str = conf.readline()
-
+pwd_in_cfg = True 
 conf.close()
 
 #-------------------------Parsing information from config.txt------------#
@@ -40,11 +40,14 @@ else:
     sys.exit("Could not get private key. Closing...")
 
 if "=" in pass_str:
-    pwd = pass_str[pass_str.find("=")+1:].split()[0]
-else:
-    sys.exit("Could not get pass. Closing...")
 
-#-------------------------------------------------------------------------#
+	try:
+		pwd = pass_str[pass_str.find("=")+1:].split()[0]
+
+	except:
+		print("SSH password not found in configs.txt")
+		pwd_in_cfg = False
+
 
 
 #------------------------Reciting parsed information for clarity----------#
@@ -54,11 +57,33 @@ print("Name is: " + name)
 print("Host is: " + host)
 print("Private key is: " + p_key)					  
 
+
+
+
+#------------------------Checking if the local folders/files exist--------#
+
+if(os.path.isdir("received") == False) or (os.path.isdir("send") == False):
+	print("Directory named (received) or (send) is missing. Run with \"linit\" argument to create both.")
+	sys.exit()
+else:
+	print("Local folders found.") 
+
+if(os.path.isfile(p_key) == False):
+	print("SSH key not found. Please make sure it points to the right directory in configs.txt")
+	sys.exit()
+else:
+	print("SSH key found.")
+
+
+
+
 #------------------------Performing specified operation-------------------#
 
-
-
 if(user_input.operation == "get"):
+
+	if(pwd_in_cfg == False):
+		pwd = input("Enter your SSH pass: ")	
+	
 	print("Input is get. Retrieving files.")
 	with pysftp.Connection(host, username=name, private_key=p_key, private_key_pass=pwd) as sftp:
 		sftp.get_r('to_local', 'received', preserve_mtime=True)        
@@ -66,10 +91,19 @@ if(user_input.operation == "get"):
 			
 elif (user_input.operation == "put"):
 	print("Input is put. Sending files.")
+
+	if(pwd_in_cfg == False):
+		pwd = input("Enter your SSH pass: ")
+
+	if (len(os.listdir("send")) == 0):								#Check if there is something to send first.
+		print("Send directory is empty. Nothing to send.")
+		sys.exit()
+
 	with pysftp.Connection(host, username=name, private_key=p_key, private_key_pass=pwd) as sftp:	#Sends both files AND folders. Function complete.
 		sftp.put_r('send', 'received', preserve_mtime=True)
 else:
 	sys.exit("Input not recognized. Argument is either get or put.")
 
+#-------------------------------------------------------------------------#
 
 
